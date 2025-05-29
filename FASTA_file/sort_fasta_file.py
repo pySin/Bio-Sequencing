1e7b9dd9455327a2716d6b27e253eb138baf3ebe
pySin <sinan_km@yahoo.com>
2025-05-29 16:49:41 +0100

    Test getopt

--- FASTA_file/sort_fasta_file.py
+++ FASTA_file/sort_fasta_file.py
@@ -1,5 +1,11 @@
-class SortFastaFile:
+import sys
+import getopt
 
+
+class SortFastaFile:
+    """
+    Create a dictionary
+    """
     def __init__(self, filename):
         self.filename = filename
         self.seqs = {}
@@ -7,7 +13,8 @@ class SortFastaFile:
     def read_fasta(self):
         try:
             f_file = open(self.filename, "r")
-        except IOError:
+            print(f"Arguments: {sys.argv}")
+        except Exception:
             print(f"{self.filename} does not exist!")
 
         for line in f_file:
@@ -21,14 +28,38 @@ class SortFastaFile:
                 self.seqs[name] = ""
             else:  # Sequence is not a header
                 self.seqs[name] += line
-        print(f"FASTA dict: {self.seqs}")
+        # print(f"FASTA dict: {self.seqs}")
         f_file.close()
 
 
+    def show_sequences(self):
+        for key, value in self.seqs.items():
+            print(f"Name: {key}")
+            print(f"Value: {value}")
+
+    def usage(self):
+        print(f"Some options connected with the arguments received from the command line.")
+
+    def arg_options(self):
+        o, a = getopt.getopt(sys.argv[1:], "l:h")
+        opts = {}
+        seqlen = 0
+
+        for k, v in o:
+            opts[k] = v
+
+        if "-h" in opts.keys():
+            self.usage(); sys.exit()
+
 
 def caller():
-    f_file = SortFastaFile("gene.fna")
+
+    print(f"Command line arguments: {sys.argv}")
+    file_name = sys.argv[1]
+    f_file = SortFastaFile(file_name)
     f_file.read_fasta()
+    # f_file.show_sequences()
+    f_file.arg_options()
 
 
 if __name__ == "__main__":
