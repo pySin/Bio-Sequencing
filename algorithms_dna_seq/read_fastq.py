2ddf44cc1871bd847809cf250c2fbabb8b05d001
pySin <sinan_km@yahoo.com>
2025-06-01 22:34:59 +0100

    Separate sequences and quality scores.

--- algorithms_dna_seq/read_fastq.py
+++ algorithms_dna_seq/read_fastq.py
@@ -6,4 +6,14 @@
 def read_fastq(filename):
     sequences = []
     qualities = []
-
+    with open(filename) as f:
+        while True:
+            f.readline()
+            seq = f.readline().rstrip()
+            f.readline()
+            qual = f.readline().rstrip()
+            if len(seq) == 0:
+                break
+            sequences.append(seq)
+            qualities.append(qual)
+    return sequences, qualities
