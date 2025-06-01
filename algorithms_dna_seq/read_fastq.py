650d5a9df1eef23a4cfb2d2cdc2f42d1b2a2afb4
pySin <sinan_km@yahoo.com>
2025-06-01 22:58:49 +0100

    Create quality histogram.

--- algorithms_dna_seq/read_fastq.py
+++ algorithms_dna_seq/read_fastq.py
@@ -23,6 +23,19 @@ def phred33ToQ(qual):
     return ord(qual) - 33
 
 
+
 seqs, quals = read_fastq("SRR835775_1.first1000.fastq")
-print(f"Sequences: {seqs[:2]}")
-print(f"Qualities: {quals[:2]}")
+# print(f"Sequences: {seqs[:2]}")
+# print(f"Qualities: {quals[:30]}")
+
+# print(phred33ToQ("J"))
+
+def create_hist(qualities):
+    hist = [0] * 50
+    for qual in qualities:
+        for phred in qual:
+            q = phred33ToQ(phred)
+            hist[q] += 1
+    return hist
+
+print(create_hist(quals))
