3af2aa0e1510a4d51d42c29d231a92555578a43d
pySin <sinan_km@yahoo.com>
2025-06-01 22:26:45 +0100

    Read FASTQ function.

--- algorithms_dna_seq/read_fastq.py
+++ algorithms_dna_seq/read_fastq.py
@@ -1,3 +1,9 @@
-with open("SRR835775_1.first1000.fastq", "r") as f:
-    for line in f:
-        print(f"Line: {line}")
+# with open("SRR835775_1.first1000.fastq", "r") as f:
+#     for line in f:
+#         print(f"Line: {line}")
+
+
+def read_fastq(filename):
+    sequences = []
+    qualities = []
+
