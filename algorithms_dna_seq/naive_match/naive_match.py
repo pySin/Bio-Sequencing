008279c5c814e35b2c3a16872a5d96162dc93838
pySin <sinan_km@yahoo.com>
2025-06-02 14:42:54 +0100

    Test matches.

--- algorithms_dna_seq/naive_match/naive_match.py
+++ algorithms_dna_seq/naive_match/naive_match.py
@@ -1,6 +1,8 @@
 # Naive match
 
 
+
+
 def read_genome(filename):
     genome = ""
     with open(filename, "r") as f:
@@ -8,3 +10,25 @@ def read_genome(filename):
             if not line[0] == ">":
                 genome += line.rstrip()
     return genome
+
+
+genome = read_genome("phix.fa")
+
+
+def naive(p, t):
+    occurrences = []
+    for i in range(len(t) - len(p) + 1):
+        match = True
+        for j in range(len(p)):
+            if not t[i + j] == p[j]:
+                match = False
+                break
+        if match:
+            occurrences.append(i)  # Append only the starting index of the match
+    return occurrences
+
+t = "AGCTTAGATAGC"
+p = "AG"
+results_match = naive(p, t)
+print(f"Matches: {results_match}")
+
