e1f4c824fff8315be6f39a7f54e731c610e045d0
pySin <sinan_km@yahoo.com>
2025-06-09 15:42:56 +0100

    Full pattern to text matches.

--- algorithms_dna_seq/k_mer_index/k_mer_index.py
+++ algorithms_dna_seq/k_mer_index/k_mer_index.py
@@ -20,3 +20,25 @@ class Index(object):
             hits.append(self.index[i][1])
             i += 1
         return hits
+
+
+def query_index(p, t, index):  # p = pattern, t = text(Genome)
+    """
+    Check if the rest of the pattern matches before authorising the offsets.
+    :param p: pattern to look for
+    :param t: text to look in (DNA sequence)
+    :param index: indexes in "t" where the first k(2) letters from "p" match
+    :return: indexes in "t" where the whole of "p" matches (not only the first 2 letters).
+    """
+    k = index.k
+    offsets = []
+    for i in index.query(p):  # returns list of possible places where matches of "p" could start
+        if p[k:] == t[i + k:i + len(p)]:
+            offsets.append(i)
+    return offsets
+
+
+t = "GCTCACGATCTAGAATCTA"
+p = "TCTA"
+print(Index(t, 2).query(p))
+print(query_index(p, t, Index(t, 2)))
