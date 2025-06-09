fd05af393238edaf9ccb782952e1b4b8f7d8f712
pySin <sinan_km@yahoo.com>
2025-06-09 02:49:50 +0100

    Create list of tuples for reads of particular length

--- algorithms_dna_seq/k_mer_index/k_mer_index.py
+++ algorithms_dna_seq/k_mer_index/k_mer_index.py
@@ -6,3 +6,6 @@ class Index(object):
     def __init__(self, t, k):
         self.k = k
         self.index = []
+        for i in range(len(t) - k + 1):
+            self.index.append((t[i:i + k], i))
+        self.index.sort()
